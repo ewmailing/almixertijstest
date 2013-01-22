58db9496444b77d4b7327c909fd0c58ae0cc3c5d
Eric Wing <ewing -dot- public |-at-| gmail.com>
2013-01-22 14:06:23 -0800

    Uses the module built by almixeriostiswigexperiment.

---
+++ Resources/ALmixer_Initialize.js
@@ -0,0 +1,101 @@
+
+var almixerproxy = require('net.playcontrol.almixerswig');
+
+var s_JSALmixerDataChannelTable = {}
+
+almixerproxy.addEventListener('ALmixerSoundPlaybackFinished', 
+	function(e)
+	{
+		
+  Ti.API.info("addEventListener name is "+e.name);
+//  Ti.API.info("handle is "+e.handle);
+  Ti.API.info("addEventListener channel is "+e.channel);
+  Ti.API.info("addEventListener source is "+e.source);
+  Ti.API.info("addEventListener completed is "+e.completed);
+
+	var which_channel = e.channel;
+	var saved_table = s_JSALmixerDataChannelTable[which_channel];
+	  Ti.API.info("addEventListener saved_table is "+saved_table);
+
+	var callback_function = saved_table.onComplete;
+	var sound_handle = saved_table.soundHandle;
+	var event_table =
+	{
+		name:"audio",
+		type:"completed",
+		channel:which_channel,
+		source:e.source,
+		completed:e.completed,
+		handle:saved_table.soundHandle,
+	};
+
+	// We can now free our saved reference
+	s_JSALmixerDataChannelTable[which_channel] = null;
+	
+	// Invoke user callback
+	if(null != callback_function)
+	{
+		callback_function(event_table);
+		event_table = null;
+		callback_function = null;
+	}
+	event_table = null;
+
+});
+
+
+function JSALmixerPlaySound(sound_handle, options_table)
+{
+	var which_channel = -1;
+	var num_loops = 0;
+	var duration = -1;
+	var on_complete = null;
+	if(null != options_table)
+	{
+		if(null != options_table.channel)
+		{
+			which_channel = options_table.channel;
+		}
+		if(null != options_table.loops)
+		{
+			num_loops = options_table.loops;
+		}
+		if(null != options_table.duration)
+		{
+			duration = options_table.duration;
+		}
+		if(null != options_table.onComplete)
+		{
+			on_complete = options_table.onComplete;
+		}
+		
+	}
+
+	var playing_channel = ALmixer.PlayChannelTimed(which_channel, sound_handle, num_loops, duration);
+	// Do only if playing succeeded
+	if(playing_channel > -1)
+	{
+		// Save the sound_handle to solve two binding problems.
+		// Problem 1: Garbage collection. The object looks like it disappears and resurrects later.
+		// We can't let the garbage collector collect the object while it is playing.
+		// So we need keep a strong reference around.
+		// Note: The sound could be playing on multiple channels, so we need to make sure we cover that case.
+		// Problem 2: ALmixer_Data for callbacks. Currently it is not clear how to get the ALmixer_Data pointer
+		// back to Javascript in a form that is usable using the Titanium callback system since it doesn't handle
+		// pointers and doesn't know anything about SWIG.
+		// The solution to both problems is to keep a global table mapping channels to sound_handles.
+		// For each playing channel, we hold a reference to the sound which keeps the object alive 
+		// (and handles the multiple channel case). 
+		// In the callback, we can retrieve the data by looking up the channel which solves the pointer problem.
+		// Problem 3: We would also like to make the API nicer for Javascript and let people pass in anonymous callback functions for each channel.
+		// This table structure will let us also keep around their callback.
+		s_JSALmixerDataChannelTable[playing_channel] = { soundHandle:sound_handle, onComplete:options_table.onComplete };
+	}
+	return playing_channel;
+
+}
+
+ALmixer.Play = JSALmixerPlaySound;
+
+
+
---
+++ Resources/app.js
@@ -0,0 +1,218 @@
+/*
+ * Single Window Application Template:
+ * A basic starting point for your application.  Mostly a blank canvas.
+ * 
+ * In app.js, we generally take care of a few things:
+ * - Bootstrap the application with any data we need
+ * - Check for dependencies like device type, platform version or network connection
+ * - Require and open our top-level UI component
+ *  
+ */
+
+
+
+//bootstrap and check dependencies
+if (Ti.version < 1.8 ) {
+	alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');	  	
+}
+
+// This is a single context application with mutliple windows in a stack
+(function() {
+	//determine platform and form factor and render approproate components
+	var osname = Ti.Platform.osname,
+		version = Ti.Platform.version,
+		height = Ti.Platform.displayCaps.platformHeight,
+		width = Ti.Platform.displayCaps.platformWidth;
+	
+	//considering tablet to have one dimension over 900px - this is imperfect, so you should feel free to decide
+	//yourself what you consider a tablet form factor for android
+	var isTablet = osname === 'ipad' || (osname === 'android' && (width > 899 || height > 899));
+	
+	var Window;
+	if (isTablet) {
+		Window = require('ui/tablet/ApplicationWindow');
+	}
+	else {
+		// Android uses platform-specific properties to create windows.
+		// All other platforms follow a similar UI pattern.
+		if (osname === 'android') {
+			Window = require('ui/handheld/android/ApplicationWindow');
+		}
+		else {
+			Window = require('ui/handheld/ApplicationWindow');
+		}
+	}
+	
+	
+var almixerproxy = require('net.playcontrol.almixerswig');
+Ti.API.info("module is => "+almixerproxy);
+Ti.include('ALmixer_Initialize.js');
+/*
+almixerproxy.addEventListener('ALmixerSoundPlaybackFinished',function(e){
+  Ti.API.info("name is "+e.name);
+//  Ti.API.info("handle is "+e.handle);
+  Ti.API.info("channel is "+e.channel);
+  Ti.API.info("source is "+e.source);
+  Ti.API.info("completed is "+e.completed);
+});
+*/
+//ALmixer.Init(0,0,0);
+var resource_dir = Ti.Filesystem.resourcesDirectory;
+//var resource_dir = Ti.Filesystem.resourcesDirectory + Ti.Filesystem.separator;
+resource_dir = resource_dir.replace(/^file:\/\/localhost/g,'');
+resource_dir = resource_dir.replace(/%20/g,' ');
+
+var full_file_path = resource_dir + "pew-pew-lei.wav";
+Ti.API.info("full_file_path is => "+full_file_path);
+
+var sound_handle_pew = ALmixer.LoadAll(full_file_path, 0);
+//var channel = ALmixer.PlayChannelTimed(-1, sound_handle, 0, -1);
+var sound_handle_note = ALmixer.LoadAll(resource_dir + "note2_aac.aac", 0);
+var music_handle = ALmixer.LoadStream(resource_dir + "background-music-aac.wav", 0, 0, 0, 0, 0);
+	
+//	Ti.API.info("channel is => "+channel);
+var options_table = { onComplete:function(e) {
+	  Ti.API.info("name is "+e.name);
+//  Ti.API.info("handle is "+e.handle);
+  Ti.API.info("channel is "+e.channel);
+  Ti.API.info("source is "+e.source);
+  Ti.API.info("completed is "+e.completed);
+	
+}
+};
+options_table.loops = -1;
+var music_channel = ALmixer.Play(music_handle, options_table);
+options_table.loops = 1;
+//var note_channel = ALmixer.Play(sound_handle_note, options_table);
+options_table.loops = 4;
+var pew_channel = ALmixer.Play(sound_handle_pew, options_table);
+/*
+music_handle = null;
+ALmixer.FreeData(sound_handle_note);
+sound_handle_note = null;
+sound_handle_pew = null;
+*/
+//	var ret_win = new Window().open();
+	var win = Window();
+
+	var note_button = Ti.UI.createButton({
+//	backgroundImage:'blue.png',
+	title:'note',
+//	width:90,
+//	height:35,
+//	right:12,
+//	bottom:10,
+	top:44,
+	left:4,
+//	font:{fontFamily:'Arial',fontWeight:'bold',fontSize:14},
+//	opacity:0
+	});
+
+	note_button.addEventListener('click', function(event)
+	{
+		var note_channel = ALmixer.Play(sound_handle_note,
+		{
+			loops:0,
+			onComplete:function(e)
+			{
+			 	Ti.API.info("completed_note is "+e.name);
+			 	Ti.API.info("name is "+e.name);
+				Ti.API.info("channel is "+e.channel);
+				Ti.API.info("source is "+e.source);
+				Ti.API.info("completed is "+e.completed);
+			},
+		});
+
+	}); 
+/*
+var window1 = Titanium.UI.createWindow({
+    
+})
+*/
+
+	var pew_button = Ti.UI.createButton({
+//	backgroundImage:'blue.png',
+	title:'pew',
+//	width:90,
+//	height:35,
+//	right:12,
+//	bottom:10,
+	top:44,
+	left:64,
+//	font:{fontFamily:'Arial',fontWeight:'bold',fontSize:14},
+//	opacity:0
+	});
+
+	pew_button.addEventListener('click', function(event)
+	{
+		var pew_channel = ALmixer.Play(sound_handle_pew,
+		{
+			loops:2,
+			onComplete:function(e)
+			{
+			 	Ti.API.info("completed pew is "+e.name);
+			 	Ti.API.info("name is "+e.name);
+				Ti.API.info("channel is "+e.channel);
+				Ti.API.info("source is "+e.source);
+				Ti.API.info("completed is "+e.completed);
+			},
+		});
+
+	}); 
+	
+	
+	var music_button = Ti.UI.createButton({
+//	backgroundImage:'blue.png',
+	title:'Music Pause',
+//	width:90,
+//	height:35,
+//	right:12,
+//	bottom:10,
+	top:4,
+	left:4,
+//	font:{fontFamily:'Arial',fontWeight:'bold',fontSize:14},
+//	opacity:0
+	});
+
+	music_button.addEventListener('click', function(event)
+	{
+
+		if(ALmixer.IsPausedChannel(music_channel))
+		{
+			ALmixer.ResumeChannel(music_channel);
+		}
+		else
+		{
+			ALmixer.PauseChannel(music_channel);
+		}
+
+
+	}); 
+	
+	var volume_slider = Titanium.UI.createSlider({
+    min:0,
+    max:1.0,
+    value:1.0,
+    width:268,
+    height:11,
+    top:90,
+ //   leftTrackImage:'../images/slider_orangebar.png',
+ //   rightTrackImage:'../images/slider_lightbar.png',
+ //   thumbImage:'../images/slider_thumb.png'
+});
+volume_slider.addEventListener('change',function(e)
+{
+	ALmixer.SetMasterVolume(e.value);
+});
+
+
+
+
+//var curr_win = Ti.UI.currentWindow;
+win.add(note_button);
+win.add(pew_button);
+win.add(music_button);
+win.add(volume_slider);
+win.open();
+
+})();
